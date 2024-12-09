8e7ec3e09d96ae1173a33d9acdfcaa900e91c145
mava1 <anamariamaldonado1994@gmail.com>
2024-12-09 10:56:37 -0500

    Hooks and Fragments

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import React from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
+const useSemiPersistentState = () => {
+  const [todoList, setTodoList] = useState(() => {
+    const savedTodoList = localStorage.getItem("savedTodoList");
+    let initialTodoList = [];
+    if (savedTodoList) {
+      try {
+        initialTodoList = JSON.parse(savedTodoList);
+      } catch (error) {
+        console.error("error parsing todo list from localStorage:", error);
+        initialTodoList = [];
+      }
+    }
+    return initialTodoList;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+};
+
 function App() {
   const [newTodo, setNewTodo] = React.useState("");
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState();
+
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
   };
   return (
-    <div>
+    <>
       <h1> To Do List</h1>
       <br />
       <AddTodoForm onAddTodo={addTodo} />
       <br />
       <TodoList todoList={todoList} />
-    </div>
+    </>
   );
 }
 
